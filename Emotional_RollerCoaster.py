cfe94c5c6b6da932708042494558bce0293ecdb5
Mohamed Washeem <79390278+mohamedwasheem@users.noreply.github.com>
2023-07-12 23:25:55 +0800

    v1.0.0.20230712

---
+++ Emotional_RollerCoaster.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 08:44:57 2023
+
+@author: User
+"""
+
+import streamlit as st
+import pandas as pd
+
+# Initialize user_ratings if it doesn't exist in session_state
+if "user_ratings" not in st.session_state:
+    st.session_state.user_ratings = []
+
+# Ask for user's name
+name = st.text_input("Enter your name:")
+
+# Read in CSV with motivational messages
+messages_df = pd.read_csv("motivational_messages.csv")
+
+# Print message with name + motivational message
+if st.button("Generate Motivational Message"):
+    # Randomly select a message from the DataFrame
+    selected_motiv = messages_df.sample().iloc[0]["Motivation"]
+    
+    # Print the message with the user's name
+    st.write(f"Hi {name}! Here's a motivational message for you:")
+    st.write(selected_motiv)
+
+# Print message with name + motivational message
+if st.button("Roast Me"):
+    # Randomly select a message from the DataFrame
+    selected_roast = messages_df.sample().iloc[0]["Demotivation"]
+    
+    # Print the message with the user's name
+    st.write(f"Hi {name}! Brace Yourself for a Roast:")
+    st.write(selected_roast)
+
+rating = st.radio("Rate your experience", options=[1, 2, 3, 4, 5])
+if st.button("Rate Your Experience"):
+    # Ask the user to rate their experience
+    
+    user_rating = {"User": name, "Rating": rating}
+    st.session_state.user_ratings.append(user_rating)
+
+    # Display the thank you message
+    st.write(f"Thank you, {name}! You rated your experience as {user_rating['Rating']} out of 5.")
+
+# Display the logged user ratings
+st.subheader("Realtime Supporters Ratings")
+try:
+    df = pd.DataFrame(st.session_state.user_ratings)
+    df.columns = ["SUPPORTERS","RATING"]
+    st.write(df)
+except:
+    st.subheader("YOU ARE THE FIRST SCAPE GOAT")
+    
+# for rating in st.session_state.user_ratings:
+#     st.write(f"User: {rating['User']}, Rating: {rating['Rating']}")
